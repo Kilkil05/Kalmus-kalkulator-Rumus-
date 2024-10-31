3a494cafb470b13773371d063f6b65d2096b7d1c
Aova Rahmadani <89490242+Kilkil05@users.noreply.github.com>
2024-10-31 12:00:16 +0700

    update diskon dan rasio

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// function formatRibuan(angka) {
+//     let str = angka.toString()
+//     let hasil = ''
+//     let counter = 0
+
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         hasil = str[i] + hasil
+//         counter++
+
+//         if (counter % 3 === 0 && i !== 0){
+//             hasil = '.' + hasil
+//         }
+//     }
+//     return hasil
+// }
+
+function formatAngka(angka){
+    return new Intl.NumberFormat('id-ID', {minimumFractionDigits: 2}).format(angka)
+}
+
 function penyederhanaanAkar () {
     let inputAkar = document.getElementById('inputakar')
     let angka = parseFloat(inputAkar.value)
@@ -20,3 +40,27 @@ function penyederhanaanAkar () {
         output.innerHTML = "<h3> Hasil Penyederhanaan dari √"+ angka +" adalah : " + akarKuadrat + "√" + sisa
     }
 }
+
+function diskon() {
+    let inputharga = document.getElementById('inputharga')
+    let inputdiskon =  document.getElementById('inputdiskon')
+    let harga = parseFloat(inputharga.value)
+    let diskon = parseFloat(inputdiskon.value)
+    let output1 = document.getElementById('output-diskon1')
+    let output2 = document.getElementById('output-diskon2')
+    let output3 = document.getElementById('output-diskon3')
+
+    var diskonBersih = harga * diskon / 100
+    var jumlah = harga - diskonBersih
+
+    const formatharga = formatAngka(jumlah)
+
+    output1.innerHTML = "<h3> " + diskon + "% dari " + harga + " yaitu : " + diskonBersih
+    output2.innerHTML = "<h3> " + harga + " - " + diskonBersih + " : " + jumlah
+    output3.innerHTML = "<h1> Total harga diskon adalah : Rp. " + formatharga
+
+
+}
+
+
+
